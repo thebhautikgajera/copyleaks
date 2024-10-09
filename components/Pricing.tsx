7ea5970a813a7f25896341938a96ddec11ac25f9
thebhautikgajera <bhautikpatel464@gmail.com>
2024-10-09 16:52:40 +0530

    5th commit

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,11 +1,4 @@
-"use client";
-
-import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import Navbar from "./Navbar";
-import { loadStripe } from '@stripe/stripe-js';
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+"use client"
 
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
