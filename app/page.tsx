fb6b62673aa25e1936a9568e9a8c282397413996
thebhautikgajera <bhautikpatel464@gmail.com>
2024-10-28 11:25:41 +0530

    36

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
-import HomeBeforeLogin from '../components/HomeBeforeLogin'
-=======
-import Home from '../components/Home'
->>>>>>> 31777ce39bafa97c4b4ffcd4957afae5aadf3caa
-
+import HomeBeforeLogin from "../components/HomeBeforeLogin";
 
 const page = () => {
   return (
     <>
-        <HomeBeforeLogin />
+      <HomeBeforeLogin />
     </>
   );
 };
