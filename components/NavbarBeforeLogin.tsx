425660afaad2da7b6759b742d007cd6dc4192a22
thebhautikgajera <bhautikpatel464@gmail.com>
2024-10-28 14:46:37 +0530

    38

--- components/NavbarBeforeLogin.tsx
+++ components/NavbarBeforeLogin.tsx
@@ -38,7 +38,7 @@ const NavbarBeforeLogin = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-transparent shadow-md relative z-50">
       <div className="flex-shrink-0">
-        <Link href="/home">
+        <Link href="/">
           <Image src={Logo} alt="Logo" width={320} height={270} />
         </Link>
       </div>
@@ -50,6 +50,9 @@ const NavbarBeforeLogin = () => {
           <div className={`fixed inset-0 bg-indigo-900 bg-opacity-95 z-40 transition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
             <div className="flex flex-col justify-center items-center h-full">
               <Link href="/" className={`block px-4 py-2 text-2xl mb-4 ${isActive('/') ? 'bg-white bg-opacity-20 font-bold' : 'text-white'}`} onClick={toggleMenu}>Home</Link>
+              <Link href="/about" className={`block px-4 py-2 text-2xl mb-4 ${isActive('/about') ? 'bg-white bg-opacity-20 font-bold' : 'text-white'}`} onClick={toggleMenu}>About</Link>
+              <Link href="/pricing" className={`block px-4 py-2 text-2xl mb-4 ${isActive('/pricing') ? 'bg-white bg-opacity-20 font-bold' : 'text-white'}`} onClick={toggleMenu}>Pricing</Link>
+              <Link href="/contact" className={`block px-4 py-2 text-2xl mb-4 ${isActive('/contact') ? 'bg-white bg-opacity-20 font-bold' : 'text-white'}`} onClick={toggleMenu}>Contact</Link>
               <Link href="/login" className={`block px-4 py-2 text-2xl mb-4 ${isActive('/login') ? 'bg-white bg-opacity-20 font-bold' : 'text-white'}`} onClick={toggleMenu}>Login</Link>
               <Link href="/register" className={`block px-4 py-2 text-2xl mb-4 ${isActive('/register') ? 'bg-white bg-opacity-20 font-bold' : 'text-white'}`} onClick={toggleMenu}>Sign Up</Link>
             </div>
@@ -58,6 +61,9 @@ const NavbarBeforeLogin = () => {
       ) : (
         <div className="flex items-center space-x-6">
           <Link href="/" className={`text-[1.3vw] px-3 py-1 rounded ${isActive('/') ? 'bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg font-bold' : 'text-white hover:text-gray-300'}`}>Home</Link>
+          <Link href="/about" className={`text-[1.3vw] px-3 py-1 rounded ${isActive('/about') ? 'bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg font-bold' : 'text-white hover:text-gray-300'}`}>About</Link>
+          <Link href="/pricing" className={`text-[1.3vw] px-3 py-1 rounded ${isActive('/pricing') ? 'bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg font-bold' : 'text-white hover:text-gray-300'}`}>Pricing</Link>
+          <Link href="/contact" className={`text-[1.3vw] px-3 py-1 rounded ${isActive('/contact') ? 'bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg font-bold' : 'text-white hover:text-gray-300'}`}>Contact</Link>
           <Link href="/login" className={`text-[1.3vw] px-3 py-1 rounded ${isActive('/login') ? 'bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg font-bold' : 'text-white hover:text-gray-300'}`}>Login</Link>
           <Link href="/register" className={`text-[1.3vw] px-3 py-1 rounded ${isActive('/register') ? 'bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg font-bold' : 'text-white hover:text-gray-300'}`}>Sign Up</Link>
         </div>
